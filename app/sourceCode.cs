502d59c547ffb89c56ca93002d8648327f356c1a
figimodi <figimodi@gmail.com>
2022-10-29 14:43:20 +0200

    sourceCode.cs upload

---
+++ app/sourceCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] fileEntries = Directory.GetFiles(args[0]);
+        string fileContent = String.Empty;
+
+        foreach (string file in fileEntries)
+        {
+            int idx = file.LastIndexOf('.');
+
+            if (args[2] == "true" && file[(idx + 1)..] != "jpg")
+            {
+                string newFile = file;
+                newFile = newFile.Replace(file[(idx + 1)..], "jpg");
+
+                File.Move(file, newFile);
+                newFile = newFile.Replace("\\", "/");
+                fileContent += "\"url(" + newFile + ")\",\n";
+                continue;
+            }
+
+            fileContent += "\"url(" + file.Replace("\\", "/") + ")\",\n";
+        }
+
+        fileContent = fileContent.Replace("..", ".");
+
+        using (StreamWriter sw = new StreamWriter(args[1]))
+        {
+            sw.Write(fileContent);
+        }
+    }
+}
